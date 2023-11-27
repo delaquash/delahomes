9d2b8e8661e2294e75fe3334e66270f64aa6261a
delaquash <olaide1191@gmail.com>
2023-11-27 12:38:19 +0100

    Feat: add redux as state management

---
+++ client/state/reducers/userSlice.tsx
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface StateProps {
+    currentUser: string | null;
+    error:string | null;
+    loading: boolean 
+}
+const initialState: StateProps = {
+    currentUser: null,
+    error: null,
+    loading: false,
+}
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.error = null;
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;      
+        }, 
+        signInFail: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
+    }
+})
+
+export const { signInStart, signInFail, signInSuccess } = userSlice.actions
+export default userSlice.reducer;
