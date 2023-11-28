2f84a36601f750a39fa25bbd958690dde48e85d3
delaquash <olaide1191@gmail.com>
2023-11-28 21:59:20 +0100

    fix: sign in page with react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from '../store.ts'
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  
   </Provider>,
 )
