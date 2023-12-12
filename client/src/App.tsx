f7b033c3435500e7308824daf4de58efc5448235
delaquash <olaide1191@gmail.com>
2023-12-12 23:18:24 +0100

    feat: add create listing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,6 +21,7 @@ const App = () => {
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="update-listing" element={<UpdateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
