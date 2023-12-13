d12c945894007724c57809c8574d747bb048d556
delaquash <olaide1191@gmail.com>
2023-12-13 11:29:19 +0100

    feat: add bolierplate for update listing page

--- client/src/pages/UpdateListing.tsx
+++ client/src/pages/UpdateListing.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react';
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { StateProps } from '../../types/dataTypes';
+
+
+export type RootState = {
+  user: StateProps;
+  // Add other slices if you have more reducers
+};
 
 const UpdateListing = () => {
+    const navigate = useNavigate();
+    const params = useParams()
+    const { currentUser } = useSelector((state: RootState) => state.user);
   return (
     <div>UpdateListing</div>
   )
