9f503a1bf8a240c369842d4f1aa4e925656b8d7e
delaquash <olaide1191@gmail.com>
2023-12-13 11:30:59 +0100

    feat: add state and formdata for update listing page

--- client/src/pages/UpdateListing.tsx
+++ client/src/pages/UpdateListing.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react';
 import {
   getDownloadURL,
@@ -20,6 +21,25 @@ const UpdateListing = () => {
     const navigate = useNavigate();
     const params = useParams()
     const { currentUser } = useSelector((state: RootState) => state.user);
+     const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+    name: '',
+    description: '',
+    address: '',
+    type: 'rent',
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 50,
+    discountPrice: 0,
+    offer: false,
+    parking: false,
+    furnished: false,
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   return (
     <div>UpdateListing</div>
   )
