6bf2707c4d2cd1e2f5450c0648db35ee5931b8ba
delaquash <olaide1191@gmail.com>
2023-12-16 21:54:11 +0100

    fix: about page

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,9 +1,19 @@
-
-
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      {" "}
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Delaquash Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Delaquash Estate is a leading real estate agency that specializes in
+        helping clients buy, sell, and rent properties in the most desirable
+        neighborhoods. Our team of experienced agents is dedicated to providing
+        exceptional service and making the buying and selling process as smooth
+        as possible.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
