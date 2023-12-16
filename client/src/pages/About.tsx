2ae38c651c4bf382fdc67186466b3c86ee73d2d8
delaquash <olaide1191@gmail.com>
2023-12-16 21:55:00 +0100

    fix: about page paragraph

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,6 +1,6 @@
 function About() {
   return (
-    <div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
       {" "}
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
         About Delaquash Estate
