91190e291c424998ab226f3c476da30d4e7e760b
delaquash <olaide1191@gmail.com>
2023-12-22 23:54:35 +0100

    feat: add listing page to route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import Headers from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 const App = () => {
   return(
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/about" element={<About />}  />
         <Route path="/signin" element={<Signin />}  />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/listing" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
