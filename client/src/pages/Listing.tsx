c1ae20f7d852e7136f5be643d3dde3e35afe6db2
delaquash <olaide1191@gmail.com>
2023-12-23 16:57:22 +0100

    feat: local state for listing page

--- client/src/pages/Listing.tsx
+++ client/src/pages/Listing.tsx
@@ -1,5 +1,9 @@
-
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 const Listing = () => {
+    const [listing, setListing] = useState(null);
+    use
   return (
     <div>Listing</div>
   )
