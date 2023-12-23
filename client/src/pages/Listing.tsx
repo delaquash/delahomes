19f58da9015a1fed492796fe65df2d9e9ea26721
delaquash <olaide1191@gmail.com>
2023-12-23 22:11:50 +0100

    feat: fetch list in listing page

--- client/src/pages/Listing.tsx
+++ client/src/pages/Listing.tsx
@@ -1,9 +1,18 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
 const Listing = () => {
     const [listing, setListing] = useState(null);
-    use
+    const params = useParams
+    useEffect(() => {
+          const fetchList = async () => {
+            const { data } = await axios.get(`http://localhost:5000/api/list/get-list/${params.listingId}`)
+              setListing(data)
+          }   
+        fetchList()
+    })
   return (
     <div>Listing</div>
   )
