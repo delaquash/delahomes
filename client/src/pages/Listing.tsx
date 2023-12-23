f34e15d128b9e05bc142605849f957c9b5cb214e
delaquash <olaide1191@gmail.com>
2023-12-23 23:10:36 +0100

    feat: listing api call in react query

--- client/src/pages/Listing.tsx
+++ client/src/pages/Listing.tsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from 'react-query';
 interface Params {
@@ -7,32 +6,22 @@ interface Params {
 }
 
 const Listing = () => {
-    const [listing, setListing] = useState(null);
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(true);
     const params: Params = useParams();
     const { listingId } = params;
-
-
     
-    useEffect(() => {
-        setLoading(true)
-        const fetchList = async () => {
-            try {
-                const { data } = await axios.get(`http://localhost:5000/api/list/get-list/${params.listingId}`)
-                if (data.success === false) {
-                    setError(true)
-                    return;
-                }
-                setListing(data)
-            } catch (error) {
-                setLoading(false)
-                setError(true)
-            }
-            
-        }
-        fetchList();
-    }, [params.listingId])
+    const { data: listing, error, isLoading } = useQuery([], async () => {
+        const { data } = await axios.get(`http://localhost:5000/api/list/get-list/${listingId}`);
+        return data;
+    });
+
+    if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+    if (error || !listing || listing.success === false) {
+    return <div>Error loading listing data.</div>;
+  }
+
   return (
       <div>{ listing && listing.name}</div>
   )
