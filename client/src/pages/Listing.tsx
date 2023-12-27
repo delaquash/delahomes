f9a211c4459d8a8d85b703d0be3093a7976a14d1
delaquash <olaide1191@gmail.com>
2023-12-27 22:36:33 +0100

    feat: add styling to list page

--- client/src/pages/Listing.tsx
+++ client/src/pages/Listing.tsx
@@ -87,7 +87,7 @@ const Listing = () => {
                 <span className="font-semibold text-black">Description - {" "}</span>
                 {listing.description}
               </p>
-              <ul>
+              <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
                 <li className='flex items-center gap-1 whitespace-nowrap '>
                   <FaBed className='text-lg' />
                     {listing.bedrooms > 1 ? `${listing.bedrooms} beds `: `${listing.bedrooms} bed `}
