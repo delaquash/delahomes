6b4f26a5dfecbecf59d1b7d516f86b8964367e4d
delaquash <olaide1191@gmail.com>
2023-12-27 03:44:07 +0100

    feat: add list description and list price

--- client/src/pages/Listing.tsx
+++ client/src/pages/Listing.tsx
@@ -73,9 +73,20 @@ const Listing = () => {
           <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
                 <p className='text-2xl font-semibold'>
                   {listing.name} - ${' '}
-
+                  {listing.offer ? listing.discountPrice.toLocaleString("en-NG")
+                    : listing.regularPrice.toLocaleString('en-NG')}
+                  {listing.type === 'rent' && ' / month'}
                 </p>
-          </div>
+                {listing.offer && (
+                  <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                    ${+listing.regularPrice - +listing.discountPrice}
+                  </p>
+                )}
+              </div>
+              <p className="text-slate-800">
+                <span className="font-semibold text-black">Description - {" "}</span>
+                {listing.description}
+              </p>
         </div>
       )}
     </>
