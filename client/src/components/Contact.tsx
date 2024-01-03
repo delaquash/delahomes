8c2e8ac32b7b677da7eca160df693eeaab2f2495
delaquash <olaide1191@gmail.com>
2024-01-03 01:00:58 +0100

    feat: add textarea to contact page

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import { useQuery } from "react-query"
-
+import { Link } from "react-router-dom";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 interface listProps {
@@ -16,6 +16,9 @@ interface LandLordProps {
 }
 const Contact = ({ listing }: listProps) => {
   const[message, setMessage ] = useState("")
+  const onChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setMessage(e.target.value);
+  };
   const {data: landlord, error} = useQuery(['landlord', listing.userRef], async () => {
     const res = await axios.get(`/api/user/${listing.userRef}`);
     return res.data as LandLordProps;
@@ -34,9 +37,19 @@ const Contact = ({ listing }: listProps) => {
             <span className='font-semibold'>{listing.name.toLowerCase()}</span>
           </p>
           <textarea
-          
-          />
-
+            name="message"
+            id="message"
+            value={message}
+            onChange={onChange}
+            placeholder='Enter your message here...'
+            className='w-full border p-3 rounded-lg'
+          ></textarea>
+          <Link
+          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+          >
+            Send Message          
+          </Link>
         </div>
       )}
     </>
