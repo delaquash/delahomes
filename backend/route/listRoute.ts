de6d206d1edcde745999358e5248fc16e60bf0fe
delaquash <olaide1191@gmail.com>
2024-01-28 22:08:28 +0100

    fix: refactor list endpoint

--- backend/route/listRoute.ts
+++ backend/route/listRoute.ts
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post("/create", verifyUser, createListing);
 router.delete("/delete/:id", verifyUser, deleteListing);
-router.put("/update/:id", verifyUser, updateListing);
+router.post("/update/:id", verifyUser, updateListing);
 router.get("/get-list/:id", verifyUser, getListing);
 router.get("/get", getList)
 
