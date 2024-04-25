9fb0a8a2d4369ae877b7dd0eedcdc7bcaefc6ec8
delaquash <olaide1191@gmail.com>
2024-04-25 15:47:04 +0100

    feat: add mobile and main navbar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-// import MobileNav from "./MobileNav";
-// import MainNav from "./MainNav";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
@@ -13,10 +13,10 @@ const Header = () => {
           Delaheats.com
         </Link>
         <div className="md:hidden">
-          {/* <MobileNav /> */}
+          <MobileNav />
         </div>
         <div className="hidden md:block">
-          {/* <MainNav /> */}
+          <MainNav />
         </div>
       </div>
     </div>
