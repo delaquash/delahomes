fcac02c07c32133ab5a45f274e343e1e6aad19b2
delaquash <olaide1191@gmail.com>
2024-05-16 14:58:05 +0100

    "Updated auth middleware to decode JWT token, fetch user from database, and attach user ID and auth0 ID to request object"

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -1,7 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import { auth } from 'express-oauth2-jwt-bearer';
-import jwt from "jsonwebtoken";
+import jwt, { Jwt } from "jsonwebtoken";
 import { errorHandler } from '../utils/errorHandler';
+import User from '../models/userModel';
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
     audience: process.env.AUTH0_URI,
@@ -9,7 +19,7 @@ export const jwtCheck = auth({
     tokenSigningAlg: process.env.AUTH0_TOKEN_SIGNING_ALG
   });
 
-export const JwtParser = (req: Request, res: Response, next: NextFunction) => {
+export const JwtParser = async (req: Request, res: Response, next: NextFunction) => {
    const { authorization } = req.headers;
    if(!authorization || !authorization.startsWith("Bearer ")) {
     return res.sendStatus(401)
@@ -18,7 +28,16 @@ export const JwtParser = (req: Request, res: Response, next: NextFunction) => {
    const token = authorization.split(" ")[1]
 
    try {
-    
+    const decoded = jwt.decode(token) as jwt.JwtPayload
+    const auth0Id = decoded.sub;
+
+    const user = await User.findOne({ auth0Id })
+
+    if(!user) {
+      return next(errorHandler(404, "User not found!!!"))
+    }
+    req.userId = user._id.toString();
+    req.auth0Id = user.auth0Id
    } catch (error) {
     return next(errorHandler(401, "User not authorized..."))
    }
