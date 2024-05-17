8004d7a16ab3bedccf73138f16609330e30fd795
delaquash <olaide1191@gmail.com>
2024-05-17 17:41:58 +0100

    feat: add express validator to route handler

--- backend/middleware/validation.ts
+++ backend/middleware/validation.ts
@@ -1,5 +1,9 @@
 import { body } from "express-validator";
 
+const handleValidationErrors = async (
+    
+)
+
 export const ExpressValidator = [
     body("name").isString().notEmpty().withMessage("Name must be a string"),
     body("addressLine1").isString().notEmpty().withMessage("AddressLine must be a string"),
