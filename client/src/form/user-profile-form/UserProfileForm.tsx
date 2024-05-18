221734624bac02cd6d324f863a59e2477b019922
delaquash <olaide1191@gmail.com>
2024-05-18 15:57:14 +0100

    "Added react-hook-form and zodResolver to UserProfileForm, implemented form schema validation and created a basic form structure"

--- client/src/form/user-profile-form/UserProfileForm.tsx
+++ client/src/form/user-profile-form/UserProfileForm.tsx
@@ -1,4 +1,8 @@
+import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+
 
 const formSchema = z.object({
     email: z.string().optional(),
@@ -11,15 +15,36 @@ const formSchema = z.object({
 type UseFormData = z.infer<typeof formSchema>;
 
 type Props = {
+    /* The `onSave` prop in the `Props` type is defining a function that takes an argument of type
+    `UseFormData` and returns `void`. This means that the `onSave` function passed to the
+    `UserProfileForm` component should accept an object that matches the shape defined by the
+    `UseFormData` type and does not return any value (void). This function is typically used to
+    handle saving the user profile data entered in the form. */
     onSave: (userprofileData: UseFormData) => void;
     isLoading: boolean;
 }
 
-const UserProfileForm = ({isLoading,onSave}: Props) => {
+const UserProfileForm = ({ isLoading, onSave }: Props) => {
+    const form = useForm<UseFormData>({
+        resolver: zodResolver(formSchema)
+    });
     return (
-        <>
-        </>
+        <Form {...form}>
+            <form 
+                className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+                onSubmit={form.handleSubmit(onSave)}
+            >
+                <div>
+                    <h2 className="font-bold text-2xl">User Profile Form</h2>
+                    <FormDescription>
+                        
+                    </FormDescription>
+                </div>
+            </form>
+        </Form>
     )
 }
 
 export default UserProfileForm;
+
+
