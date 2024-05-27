e6b4e0f17800f1d71d60fb65f67c8f47d843424c
delaquash <olaide1191@gmail.com>
2024-05-27 15:30:56 +0100

    "Added useGetMyUser hook and updated imports in myUserApi.tsx"

--- client/src/helper/myUserApi.tsx
+++ client/src/helper/myUserApi.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQueries, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -69,4 +69,31 @@ export const useUpdateMyUser =() => {
     return {
         updateUser, isLoading, isError, isSuccess, error, reset,
     }
+}
+
+
+export const useGetMyUser = () => {
+    const { getAccessTokenSilently } = useAuth0();
+
+    const getMyUserProfile = async () => {
+        const accessToken = getAccessTokenSilently()
+        const res = await fetch (`${API_BASE_URL}/api/v1/yser`, {
+            method: "GET",
+            headers: {
+                Authorization:  `Bearer ${accessToken}`,
+                "Content-type": "application/json",
+            },
+        })
+        if(!res.ok){
+            throw new Error("Failed to fetch User")
+        }
+        return res.json()
+    }
+
+    const {data: CurrentUser, isLoading, error, } = useQuery("fetchMyCurrentUser", getMyUserProfile)
+
+    if(error) {
+        toast.error(error.toString())
+    }
+    return { CurrentUser, isLoading}
 }
