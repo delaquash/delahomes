adf6af822de591278420fa0f625baf7d10b565fc
delaquash <olaide1191@gmail.com>
2024-05-27 14:05:28 +0100

    "Added toast notifications for success and error cases in useUpdateMyUser hook"

--- client/src/helper/myUserApi.tsx
+++ client/src/helper/myUserApi.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -57,6 +58,14 @@ export const useUpdateMyUser =() => {
  
     }
     const { mutateAsync:updateUser, isLoading, isError, error, reset, isSuccess} = useMutation(updateMyUserProfileRequest);
+    if(isSuccess){
+        toast.success("User updated successfully.")
+    }
+
+    if(error) {
+        toast.error(error.toString())
+        reset()
+    }
     return {
         updateUser, isLoading, isError, isSuccess, error, reset,
     }
