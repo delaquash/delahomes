c3488355d99c1d5495b1c5a28d1be290ca05cad4
delaquash <olaide1191@gmail.com>
2024-05-31 15:24:21 +0100

    "Refactored image upload logic in listController.ts, removed uploadImage function and integrated its logic into createRestaurant function"

--- backend/controllers/listController.ts
+++ backend/controllers/listController.ts
@@ -19,27 +19,40 @@ export const createRestaurant = async(req: Request, res: Response, next: NextFun
       if(existingUser){
         return next(errorHandler(409, "User restaurant already exist..."))
       }
-      const imageUrl = await uploadImage(req.file as Express.Multer.File);
+      // const imageUrl = await uploadImage(req.file as Express.Multer.File);
 
+      // const restaurant = new Restaurant(req.body);
+      // restaurant.imageUrl = uploadeResponse.url;
+      // restaurant.user = new mongoose.Types.ObjectId(req.userId);
+      // restaurant.lastUpdated = new Date();
+
+      const image = req.file as Express.Multer.File;
+      if (!image) {
+        return res.status(400).json({ message: "Image file is required" });
+      }
+      const base64Image = Buffer.from(image.buffer).toString("base64");
+      const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+      const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
       const restaurant = new Restaurant(req.body);
-      restaurant.imageUrl = imageUrl;
+      restaurant.imageUrl = uploadResponse.url;
       restaurant.user = new mongoose.Types.ObjectId(req.userId);
       restaurant.lastUpdated = new Date();
       await restaurant.save();
       res.status(201).send(restaurant);
         } catch (error) {
-      next(errorHandler(500, "Server error..."))
+          console.log(error);
+          res.status(500).json({ message: "Something went wrong" });
     }
 }
 
 
 
 
-  const uploadImage = async (file: Express.Multer.File) => {
-    const image = file;
-    const base64Image = Buffer.from(image.buffer).toString("base64");
-    const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+  // const uploadImage = async (file: Express.Multer.File) => {
+  //   const image = file;
+  //   const base64Image = Buffer.from(image.buffer).toString("base64");
+  //   const dataURI = `data:${image.mimetype};base64,${base64Image}`;
   
-    const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
-    return uploadResponse.url;
-  };
+  //   const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+  //   return uploadResponse.url;
+  // };
