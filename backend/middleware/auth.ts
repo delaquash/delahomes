76913c3285bfdba763c06b7d6540ecfdc2466a6e
delaquash <olaide1191@gmail.com>
2024-06-10 15:28:56 +0100

    "Rename environment variables from VITE_AUTH0_* to AUTH0_* in auth middleware"

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -14,9 +14,9 @@ declare global {
 }
 
 export const jwtCheck = auth({
-    audience: process.env.VITE_AUTH0_URI,
-    issuerBaseURL: process.env.VITE_AUTH0_ISSUER_BASE_URL,
-    tokenSigningAlg: process.env.VITE_AUTH0_TOKEN_SIGNING_ALG
+    audience: process.env.AUTH0_URI,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    tokenSigningAlg: process.env.AUTH0_TOKEN_SIGNING_ALG
   });
 
 
