9c505e81373f007b841c6f262439670d2a8904da
delaquash <olaide1191@gmail.com>
2024-06-11 14:13:59 +0100

    "Added validation middleware for MyRestaurantRequest and updated MyRestaurantRoute to use it"

--- backend/route/MyRestaurantRoute.ts
+++ backend/route/MyRestaurantRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import UploadImage from "../utils/CloudinaryStorage";
 import { createRestaurant } from "../controllers/listController";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-router.post("/", UploadImage.single("imageFile"), createRestaurant)
+router.post("/", UploadImage.single("imageFile"), validateMyRestaurantRequest, createRestaurant)
 
 export default router;
