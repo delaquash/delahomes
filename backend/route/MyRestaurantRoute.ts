7fad8a290ee38c0c78262dc72c58c8816a627679
delaquash <olaide1191@gmail.com>
2024-06-12 12:38:15 +0100

    Added jwtCheck and JwtParse middleware to MyRestaurantRoute POST endpoint.

--- backend/route/MyRestaurantRoute.ts
+++ backend/route/MyRestaurantRoute.ts
@@ -2,9 +2,10 @@ import express from "express";
 import UploadImage from "../utils/CloudinaryStorage";
 import { createRestaurant } from "../controllers/listController";
 import { validateMyRestaurantRequest } from "../middleware/validation";
+import { jwtCheck, JwtParse } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", UploadImage.single("imageFile"), validateMyRestaurantRequest, createRestaurant)
+router.post("/", UploadImage.single("imageFile"), validateMyRestaurantRequest, jwtCheck,JwtParse, createRestaurant)
 
 export default router;
