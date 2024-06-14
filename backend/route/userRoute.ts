cb4b0a314bffb01527d65d50fa249c3ed997cf83
delaquash <olaide1191@gmail.com>
2024-06-14 10:53:32 +0100

    "Removed imports and commented out auth configuration and routes"

--- backend/route/userRoute.ts
+++ backend/route/userRoute.ts
@@ -1,19 +1,19 @@
 import express from "express";
-// import {createCurrentUser,  updateCurrentUser, getCurrentUser} from "../controllers/userController";
+import { createCurrentUser } from "../controllers/userController";
 import {  jwtParse } from "../middleware/auth";
 import { ExpressValidator } from "../middleware/validation";
 const router = express.Router();
 import { auth } from 'express-oauth2-jwt-bearer';
 
-export const jwtCheck = auth({
-    audience: "mern-food-ordering-app-api",
-    issuerBaseURL: "https://dev-m6fiy401keas0g76.us.auth0.com/",
-    tokenSigningAlg:  "RS256"
+// export const jwtCheck = auth({
+//     audience: "mern-food-ordering-app-api",
+//     issuerBaseURL: "https://dev-m6fiy401keas0g76.us.auth0.com/",
+//     tokenSigningAlg:  "RS256"
     
-  });
+//   });
 
 // router.get("/",   jwtParse, getCurrentUser)
-// router.post("/createuser", jwtParse, createCurrentUser )
+router.post("/createuser",  createCurrentUser )
 // router.put("/",jwtCheck, ExpressValidator, updateCurrentUser);
 
 
