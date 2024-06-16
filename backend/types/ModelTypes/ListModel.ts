0cc33775f04d395cbdb97a1e361a27dee84e8a52
delaquash <olaide1191@gmail.com>
2024-06-16 18:11:57 +0100

    "Refactor ListModel.ts: rename interface, add new interfaces MenuItem and Restaurant, and update properties"

--- backend/types/ModelTypes/ListModel.ts
+++ backend/types/ModelTypes/ListModel.ts
@@ -1,15 +1,21 @@
-export interface listModelProps {
+import mongoose from "mongoose";
+
+interface MenuItem {
+  _id: mongoose.Schema.Types.ObjectId;
   name: string;
-  description: string;
-  address: string;
-  regularPrice: number;
-  discountPrice: number;
-  bathrooms: number;
-  bedrooms: number;
-  furnished: boolean;
-  parking: boolean;
-  type: string;
-  offer: boolean;
-  imageUrls: string[];
-  userRef: string;
+  price: number;
+}
+
+interface Restaurant {
+  _id: mongoose.Schema.Types.ObjectId;
+  user: mongoose.Schema.Types.ObjectId; // Reference to User model
+  restaurantName: string;
+  city: string;
+  country: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[]; // Array of strings
+  menuItems: MenuItem[]; // Array of MenuItem objects
+  imageUrl: string;
+  lastUpdated: Date;
 }
