3f0b7546806dd4cee10ec4a62c9aa9696a6eb35e
delaquash <olaide1191@gmail.com>
2024-06-16 17:22:27 +0100

    "Updated userRoute.ts: changed route path from '/user/:id' to '/:id' in getUser endpoint"

--- backend/route/userRoute.ts
+++ backend/route/userRoute.ts
@@ -2,11 +2,12 @@ import express from "express";
 import { createCurrentUser, updateUser, getCurrentUser, getUser } from "../controllers/userController";
 import {  jwtParse } from "../middleware/auth";
 import { ExpressValidator } from "../middleware/validation";
+
 const router = express.Router();
 
 router.post("/createuser", ExpressValidator, jwtParse,  createCurrentUser );
 router.put("/updateuser/:userId", jwtParse, ExpressValidator, updateUser);
 router.get("/getuser/:userId", jwtParse, ExpressValidator, getCurrentUser);
-router.get("/user/:id", jwtParse, ExpressValidator, getUser);
+router.get("/:id", jwtParse, ExpressValidator, getUser);
 
 export default router;
