a42b22a1d5465349a2d1268dde66c2593942c2d8
delaquash <olaide1191@gmail.com>
2024-06-16 15:56:12 +0100

    "Removed getCurrentUser function and its export"

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -109,26 +109,7 @@ const updateUser = async (req: Request, res: Response, next: NextFunction) => {
 //   }
 // }
 
-// const getCurrentUser = async (req: Request, res: Response) => {
-//   try {
-//     const currentUser = await User.findOne({ _id: req.userId });
-//     if (!currentUser) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
 
-//     res.json(currentUser);
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({ message: "Something went wrong" });
-//   }
-// };
-
-
-// // export default {
-// //   getCurrentUser,
-// //   createCurrentUser,
-// //   updateCurrentUser,
-// // };
 
 export {  
 // getCurrentUser,
