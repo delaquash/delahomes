48d028b6026c843c8f3f9b57a28ad104dea6f88d
delaquash <olaide1191@gmail.com>
2024-06-17 05:52:40 +0100

    "Commented out createRestaurant function in RestaurantController and its corresponding route in MyRestaurantRoute, added isAdmin field to userModel"

--- backend/controllers/RestaurantController.ts
+++ backend/controllers/RestaurantController.ts
@@ -13,41 +13,41 @@ import cloudinary from "cloudinary";
   }
 
 
-export const createRestaurant = async(req: Request, res: Response, next: NextFunction) => {
-    try {
-      const existingUser = await Restaurant.findOne({ user: req.userId });
-      if(existingUser){
-        return next(errorHandler(409, "User restaurant already exist..."))
-      }
-      // const imageUrl = await uploadImage(req.file as Express.Multer.File);
+// export const createRestaurant = async(req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const existingUser = await Restaurant.findOne({ user: req.userId });
+//       if(existingUser){
+//         return next(errorHandler(409, "User restaurant already exist..."))
+//       }
+//       // const imageUrl = await uploadImage(req.file as Express.Multer.File);
 
-      // const restaurant = new Restaurant(req.body);
-      // restaurant.imageUrl = uploadeResponse.url;
-      // restaurant.user = new mongoose.Types.ObjectId(req.userId);
-      // restaurant.lastUpdated = new Date();
+//       // const restaurant = new Restaurant(req.body);
+//       // restaurant.imageUrl = uploadeResponse.url;
+//       // restaurant.user = new mongoose.Types.ObjectId(req.userId);
+//       // restaurant.lastUpdated = new Date();
 
-      const image = req.file as Express.Multer.File;
-      if (!image) {
-        return res.status(400).json({ message: "Image file is required" });
-      }
-      const base64Image = Buffer.from(image.buffer).toString("base64");
-      const dataURI = `data:${image.mimetype};base64,${base64Image}`;
-      const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
-      const restaurant = new Restaurant(req.body);
-      restaurant.imageUrl = uploadResponse.url;
-      restaurant.user = new mongoose.Types.ObjectId(req.userId);
-      restaurant.lastUpdated = new Date();
-      await restaurant.save();
-      res.status(201).send(restaurant);
-        } catch (error: any) {
-          console.log(error);
-          if (error.name === 'TimeoutError') {
-            res.status(499).json({ message: 'Request Timeout' });
-          } else {
-            res.status(500).json({ message: "Something went wrong" });
-          }
-    }
-}
+//       const image = req.file as Express.Multer.File;
+//       if (!image) {
+//         return res.status(400).json({ message: "Image file is required" });
+//       }
+//       const base64Image = Buffer.from(image.buffer).toString("base64");
+//       const dataURI = `data:${image.mimetype};base64,${base64Image}`;
+//       const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
+//       const restaurant = new Restaurant(req.body);
+//       restaurant.imageUrl = uploadResponse.url;
+//       restaurant.user = new mongoose.Types.ObjectId(req.userId);
+//       restaurant.lastUpdated = new Date();
+//       await restaurant.save();
+//       res.status(201).send(restaurant);
+//         } catch (error: any) {
+//           console.log(error);
+//           if (error.name === 'TimeoutError') {
+//             res.status(499).json({ message: 'Request Timeout' });
+//           } else {
+//             res.status(500).json({ message: "Something went wrong" });
+//           }
+//     }
+// }
 
 
 
--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     password: {
       type: String,
     },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
     address: {
       type: String,
     },
