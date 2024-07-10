9a6bece89ed32a17330a9e3b75e86269c6a6507b
delaquash <olaide1191@gmail.com>
2024-07-10 16:46:14 +0100

    "Removed jwtParse import and updated auth middleware imports in userRoute.ts"

--- backend/route/userRoute.ts
+++ backend/route/userRoute.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { activateUser, RegisterUser } from "../controllers/userController";
-<<<<<<< HEAD
+
 // import {   jwtParse } from "../middleware/auth";
-=======
-import {  admin, jwtParse } from "../middleware/auth";
->>>>>>> parent of b37e429 ("Refactored authController, auth middleware, and routes to update authentication logic and token verification")
+
+import {  admin } from "../middleware/auth";
+
 import { ExpressValidator } from "../middleware/validation";
 
 const router = express.Router();
