50f4b7d9dfa8659aed2d8f65637c02d7e4736a84
delaquash <olaide1191@gmail.com>
2024-07-13 16:39:22 +0100

    "Updated auth middleware to log decoded token and user from Redis, and refactored user service to retrieve user by ID from Redis instead of MongoDB."

--- backend/services/user.service.ts
+++ backend/services/user.service.ts
@@ -1,25 +1,23 @@
 import { Response } from "express";
-import User from "../models/userModel";
+import { redis } from "../utils/redis";
 
-// get user by ID
+
 /**
- * The function `getUserByID` retrieves a user by their ID and returns the user data if found, or a
- * "User not found" message if not found.
- * @param {string} id - The `id` parameter is a string representing the unique identifier of a user
- * that you want to retrieve from the database.
+ * The function `getUserByID` retrieves a user by ID from a Redis database and sends a JSON response
+ * with the user data if found.
+ * @param {string} id - The `id` parameter is a string representing the unique identifier of the user
+ * whose information we want to retrieve.
  * @param {Response} res - The `res` parameter in the `getUserByID` function is typically used to send
- * the HTTP response back to the client. It is an instance of the `Response` object, which is provided
- * by the Express.js framework in Node.js. This object allows you to send various HTTP responses such
- * as status
- * @returns If the user is not found, a response with status code 404 and a JSON object containing the
- * message "User not found" is being returned. If the user is found, a response with status code 201
- * and a JSON object containing the success status and user data is being returned.
+ * a response back to the client making the request. It is commonly an instance of the `Response`
+ * object from a web framework like Express.js. By using `res.status(201).json()`, the function is
  */
 export const getUserByID = async (id: string, res: Response) => {
-    const user = await User.findById(id);
-    if (!user) return res.status(404).json({ message: "User not found"})
-        res.status(201).json({
-            success: true,
-            data: user
+  const userJSON = await redis.get(id);
+  if (userJSON) {
+    const user = JSON.parse(userJSON);
+    res.status(201).json({
+      success: true,
+      user,
     });
-}
+  }
+};
