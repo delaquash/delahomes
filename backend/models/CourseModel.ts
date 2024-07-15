75843c31f1c1df72c58c2789c60ec49e1a05faf2
delaquash <olaide1191@gmail.com>
2024-07-15 14:28:36 +0100

    "Added courseSchema and CourseModel to CourseModel.ts"

--- backend/models/CourseModel.ts
+++ backend/models/CourseModel.ts
@@ -80,3 +80,60 @@ const courseDataSchema = new Schema<ICourseData>({
   links: [linkSchema],
   description: String
 })
+
+const courseSchema = new Schema<ICourse>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  estimatedPrice: {
+    type: Number,
+  },
+  courseData: [courseDataSchema],
+  thumbnail :{
+    public_id: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    tags: {
+      type: String,
+      required: true
+    },
+    level:{
+      type: String,
+      required: true
+    },
+    demoUrl: {
+      type: String,
+      required: true
+    },
+    benefits: [{title: String}],
+    prerequisites:[{title: String}],
+    reviews: [reviewSchema],
+    courseData: [courseDataSchema],
+    ratings: {
+      type: Number,
+      default: 0
+    },
+    purchased: {
+      type: Number,
+      default: 0
+    }
+  }
+});
+
+const CourseModel: Model<ICourse> = mongoose.model("Course", courseSchema);
+
+export default CourseModel;
