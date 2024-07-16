5c720441d7ca310c019199a407a1a8bc4f2775b3
delaquash <olaide1191@gmail.com>
2024-07-16 13:03:52 +0100

    "Added authentication middleware to /create-course route in CourseRoute.ts"

--- backend/route/CourseRoute.ts
+++ backend/route/CourseRoute.ts
@@ -2,8 +2,9 @@ import express from "express";
 import UploadImage from "../utils/CloudinaryStorage";
 import { uploadCourse } from "../controllers/CourseController";
 import { validateMyCourseRequest } from "../middleware/validation";
+import { isUserAuthenticated, authorization } from "../middleware/auth";
 // import { jwtParse } from "../middleware/auth";
 
 const router = express.Router();
-
+router.post("/create-course", isUserAuthenticated, authorization("admin"))
 export default router;
