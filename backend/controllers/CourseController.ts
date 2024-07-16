15e7fb68f8b34e43a1964a7e8ccf777339a6fc56
delaquash <olaide1191@gmail.com>
2024-07-16 10:07:42 +0100

    "Added closing bracket to uploadCourse function"

--- backend/controllers/CourseController.ts
+++ backend/controllers/CourseController.ts
@@ -6,11 +6,11 @@ import cloudinary from "cloudinary";
 import { CatchAsyncError } from "../middleware/CatchAsyncError";
 
 // To upload a course
-export const uploadCourse = CatchAsyncError(async(req: Request, res:Response, next:NextFunction))=> {
+export const uploadCourse = CatchAsyncError(async(req: Request, res:Response, next:NextFunction)=> {
     try {
         
     } catch (error: any) {
         return next (new ErrorHandler(error.message, 500))
         
     }
-}
+})
