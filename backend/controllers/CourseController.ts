d53a398acec11817961f96879cc51507cfaef2f1
delaquash <olaide1191@gmail.com>
2024-07-17 14:22:19 +0100

    "Added editCourse functionality to CourseController and CourseRoute, allowing admins to edit courses with thumbnail upload"

--- backend/controllers/CourseController.ts
+++ backend/controllers/CourseController.ts
@@ -29,7 +29,31 @@ export const uploadCourse = CatchAsyncError(async(req: Request, res:Response, ne
 
 export const editCourse = CatchAsyncError(async(req: Request, res: Response, next: NextFunction)=> {
     try {
-        
+        const data = req.body;
+        const thumbnail = data.thumbnail;
+
+        if(thumbnail){
+             await cloudinary.v2.uploader.destroy(thumbnail.public_id)
+            const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+                folder: "courses"
+                });
+                data.thumbnail = {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url
+                }
+            }
+        const courseID = req.params.id;
+        const course = await CourseModel.findByIdAndUpdate(courseID, {
+            $set: data,
+            }, 
+            {
+                new: true,
+            },
+        )
+        res.status(200).json({
+            success: true,
+            course
+            })
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 500))
     }
