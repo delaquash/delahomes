19f54b465bc7459e46539a4c931dde096ab5752c
delaquash <olaide1191@gmail.com>
2024-07-17 14:22:19 +0100

    "Added editCourse functionality to CourseController and CourseRoute, allowing admins to edit courses with thumbnail upload"

--- backend/route/CourseRoute.ts
+++ backend/route/CourseRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import UploadImage from "../utils/CloudinaryStorage";
-import { uploadCourse } from "../controllers/CourseController";
+import { uploadCourse, editCourse } from "../controllers/CourseController";
 import { validateMyCourseRequest } from "../middleware/validation";
 import { isUserAuthenticated, authorization } from "../middleware/auth";
 // import { jwtParse } from "../middleware/auth";
 
 const router = express.Router();
-router.post("/create-course", isUserAuthenticated, authorization("admin"), uploadCourse)
+router.post("/create-course", isUserAuthenticated, authorization("admin"), uploadCourse);
+router.put("/edit-course/:id", isUserAuthenticated, authorization("admin"), editCourse)
 export default router;
