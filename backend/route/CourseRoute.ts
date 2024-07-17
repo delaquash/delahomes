89b7079bc18d99f5430320939ae75943256dd6ef
delaquash <olaide1191@gmail.com>
2024-07-17 16:20:28 +0100

    "Added Redis caching to CourseController, implemented getSingleCourse and getAllCourse endpoints, and updated CourseRoute to include new endpoints"

--- backend/route/CourseRoute.ts
+++ backend/route/CourseRoute.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import UploadImage from "../utils/CloudinaryStorage";
-import { uploadCourse, editCourse } from "../controllers/CourseController";
+import { uploadCourse, editCourse, getSingleCourse, getAllCourse } from "../controllers/CourseController";
 import { validateMyCourseRequest } from "../middleware/validation";
 import { isUserAuthenticated, authorization } from "../middleware/auth";
 // import { jwtParse } from "../middleware/auth";
 
 const router = express.Router();
 router.post("/create-course", isUserAuthenticated, authorization("admin"), uploadCourse);
-router.put("/edit-course/:id", isUserAuthenticated, authorization("admin"), editCourse)
+router.put("/edit-course/:id", isUserAuthenticated, authorization("admin"), editCourse);
+router.get("/single-course/:id", getSingleCourse);
+router.get("/get-courses", getAllCourse);
 export default router;
