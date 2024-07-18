02a563589ad1461ce1074bf47997ecfaa65a3748
delaquash <olaide1191@gmail.com>
2024-07-18 14:40:52 +0100

    "Added console logs to CourseController, changed response data in getCourseByUser, and added new route for getting course content by user"

--- backend/controllers/CourseController.ts
+++ backend/controllers/CourseController.ts
@@ -125,21 +125,24 @@ export const getCourseByUser = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const courseID = req.params.id; // Extract course ID from request parameters
+      console.log(courseID, "this is course ID")
       const userCourseList = req.user?.courses; // Retrieve user's course list from req.user in auth.ts
-
+      console.log(userCourseList, "this is user course list")
         // Check if the user has access to the course
       const isCourseExist = userCourseList?.find(
         (course: any) => course._id.toString() === courseID
       );
+      console.log(isCourseExist,"This is course exist")
       // If the course is not in the user's course list, return an error
       if(!isCourseExist){
         return next(new ErrorHandler("You are not eligible to access this course", 404));
       };
       // Find and return the course details
       const course = await CourseModel.findById(courseID);
+      const content = course?.courseData;
       res.status(200).json({
         success: true,
-        course
+        content
         });
     } catch (error: any) {
       next(new ErrorHandler(error.message, 500));
