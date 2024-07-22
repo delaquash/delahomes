4ce2d5a108edac00e5135f8c49e4185beda5df6f
delaquash <olaide1191@gmail.com>
2024-07-22 16:43:13 +0100

    "Updated CourseController to calculate average rating and create notification on review addition, and added addReview route to CourseRoute"

--- backend/route/CourseRoute.ts
+++ backend/route/CourseRoute.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import UploadImage from "../utils/CloudinaryStorage";
-import { uploadCourse, editCourse, getSingleCourse, getAllCourse, getCourseByUser, addQuestion, addAnswer } from "../controllers/CourseController";
+import { uploadCourse, editCourse, getSingleCourse, getAllCourse, getCourseByUser, addQuestion, addAnswer, addReview } from "../controllers/CourseController";
 import { validateMyCourseRequest } from "../middleware/validation";
 import { isUserAuthenticated, authorization } from "../middleware/auth";
 // import { jwtParse } from "../middleware/auth";
@@ -12,5 +12,6 @@ router.get("/single-course/:id", getSingleCourse);
 router.get("/get-courses", getAllCourse);
 router.get("/user-course-content/:id", isUserAuthenticated, getCourseByUser);
 router.post("/add-question",isUserAuthenticated, addQuestion);
-router.post("/add-answer", isUserAuthenticated, addAnswer)
+router.post("/add-answer", isUserAuthenticated, addAnswer);
+router.put("/add-review", isUserAuthenticated, addReview);
 export default router;
