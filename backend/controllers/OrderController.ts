d00661c31876523ec3dc50d3018a1da29ed48e6b
delaquash <olaide1191@gmail.com>
2024-07-24 22:41:42 +0100

    feat: add createorder route

--- backend/controllers/OrderController.ts
+++ backend/controllers/OrderController.ts
@@ -6,10 +6,23 @@ import CourseModel from "../models/CourseModel";
 import cloudinary from "cloudinary";
 import { CatchAsyncError } from "../middleware/CatchAsyncError";
 import { redis } from "../utils/redis";
+import { IOrder } from "../models/OrderModel"; 
 import sendEmail from "../utils/SendMail";
+import User from "../models/userModel";
 // import ejs from "ejs";
 // import path from "path";
 
-export interface IOrder {
+
+
+export const createOrder = CatchAsyncError(async(req: Request, res: Response, next: NextFunction)=> {
+    const {courseID, payment_info} = req.body as IOrder;
+
+    /* The line `const user = await User.findById(req.user?._id);` 
+    is querying the database to find a user based on the `_id` 
+    property of the `req.user` object. */
+    const user = await User.findById(req.user?._id);
+
     
-}
+
+
+})
