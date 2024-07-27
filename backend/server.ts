2113d1e1a9d1a25caa0112e5661b9a349e9556df
delaquash <olaide1191@gmail.com>
2024-07-27 18:08:01 +0100

    "Removed console logs and added express.json() middleware in multiple files"

--- backend/server.ts
+++ backend/server.ts
@@ -18,9 +18,10 @@ import ErrorHandler from "./utils/errorHandler";
 
 const app = express();
 app.use(cookieParser());
+app.use(express.json());
 app.use(bodyParser.json({ limit: "50mb" }));
 
-app.use(express.json());
+
 
 app.use(cors({
   origin: process.env.CLIENT_URL,
