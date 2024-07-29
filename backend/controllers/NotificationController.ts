bdc9c8cdee16ee4c81e0df22d88468fa611fb1bd
delaquash <olaide1191@gmail.com>
2024-07-29 16:20:36 +0100

    "Updated NotificationController to add updateNotification function and modified NotificationRoute to include update-notification endpoint"

--- backend/controllers/NotificationController.ts
+++ backend/controllers/NotificationController.ts
@@ -13,7 +13,31 @@ import { IOrder } from "../models/OrderModel";
 export const getNotifications = CatchAsyncError(async(req: Request, res: Response, next: NextFunction) => {
     try {
         const notification = await NotificationModel.find().sort({ createdAt: -1 });
-        res.status(200).json({ success: true, data: notification });
+        res.status(200).json({ 
+            success: true, 
+            notification 
+        });
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+});
+
+export const updateNotification = CatchAsyncError(async(req: Request, res: Response, next: NextFunction)=> {
+    try {
+        const notification = await NotificationModel.findById(req.params.id)
+        if(!notification){
+            return next(new ErrorHandler('Notification not found', 404))
+        } else {
+            notification.status ? notification.status = "read" : notification.status
+        }
+
+        await notification.save();
+
+        const notifications = await NotificationModel.find().sort({ createdAt: -1 })
+        res.status(200).json({ 
+            success: true, 
+            notifications 
+        });
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 500));
       }
--- backend/route/NotificationRoute.ts
+++ backend/route/NotificationRoute.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { getNotifications } from "../controllers/NotificationController";
+import { getNotifications, updateNotification } from "../controllers/NotificationController";
 import { authorization, isUserAuthenticated } from "../middleware/auth";
 const router = express.Router();
 
 router.get("/get-all-notifications", isUserAuthenticated, authorization("admin"), getNotifications)
+router.put("/update-notification", isUserAuthenticated, authorization("admin"), updateNotification)
 export default router;
