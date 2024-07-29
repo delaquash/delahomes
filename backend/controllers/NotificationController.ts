dd12787e1fdabdfbc39c7acc78a55c6cb80f1d48
delaquash <olaide1191@gmail.com>
2024-07-29 15:20:20 +0100

    "Added NotificationController and updated OrderModel"

---
+++ backend/controllers/NotificationController.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/errorHandler";
+import mongoose, { Mongoose } from "mongoose";
+import CourseModel from "../models/CourseModel";
+import NotificationModel from "../models/NotificationModel";
+import cloudinary from "cloudinary";
+import { CatchAsyncError } from "../middleware/CatchAsyncError";
+import { redis } from "../utils/redis";
+import { IOrder } from "../models/OrderModel"; 
+
+
+
+export const getNotifications = CatchAsyncError(async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const notification = await NotificationModel.find().sort({ createdAt: -1 })
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+})
