92644eb8809c32ec5b7b9de058632f599cc29455
delaquash <olaide1191@gmail.com>
2024-07-29 15:20:20 +0100

    "Added NotificationController and updated OrderModel"

--- backend/models/OrderModel.ts
+++ backend/models/OrderModel.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Model, Document } from "mongoose";
 
 export interface IOrder extends Document {
   courseId:string;
-  userID: string;
+  userId: string;
   payment_info: object
 }
 
@@ -12,7 +12,7 @@ const orderSchema = new Schema<IOrder> ({
     type: String,
     required: true
   },
-  userID: {
+  userId: {
     type: String,
     required: true
     },
