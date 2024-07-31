63c6a5a13df826410c7aad42c668bb63a54907df
delaquash <olaide1191@gmail.com>
2024-07-31 13:41:31 +0100

    "Formatted code in CourseModel.ts and OrderModel.ts to add trailing commas and consistent indentation."

---
+++ backend/models/LayoutModel.ts
@@ -0,0 +1,70 @@
+require("dotenv").config();
+import mongoose, { Schema, model, Document } from "mongoose";
+
+interface FaqItem extends Document {
+    question: string;
+    answer: string;
+}
+
+interface Category extends Document {
+    title: string;
+}
+
+interface BannerImage extends Document {
+    public_id: string;
+    url: string
+}
+
+interface Layout extends Document {
+    type: string;
+    faq: FaqItem[];
+    categories: Category[];
+    banner: {
+        image: BannerImage;
+        title: string;
+        subTitile: string;
+    }
+}
+
+const faqSchema = new Schema<FaqItem>({
+    question: {
+        type: String
+    },
+    answer: {
+        type: String
+    }
+});
+
+const categorySchema = new Schema<Category>({
+    title: {
+        type: String,
+    }
+})
+
+const bannerImageSchema = new Schema <BannerImage> ({
+    public_id: {
+        type: String
+        },
+    url: {
+            type: String
+        }
+})
+
+const layoutSchema = new Schema ({
+    type: {
+        type: String
+        },
+    faq: [faqSchema],
+    categories: [categorySchema],
+    banner: {
+        image: bannerImageSchema,
+        title: {
+            type: String
+            },
+            subTitile: {
+                type: String
+                }
+    }
+})
+
+export const LayoutModel = model("Layout", layoutSchema)
--- backend/models/OrderModel.ts
+++ backend/models/OrderModel.ts
@@ -2,26 +2,29 @@ require("dotenv").config();
 import mongoose, { Schema, Model, Document } from "mongoose";
 
 export interface IOrder extends Document {
-  courseId:string;
+  courseId: string;
   userId: string;
-  payment_info: object
+  payment_info: object;
 }
 
-const orderSchema = new Schema<IOrder> ({
-  courseId: {
-    type: String,
-    required: true
-  },
-  userId: {
-    type: String,
-    required: true
+const orderSchema = new Schema<IOrder>(
+  {
+    courseId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
     },
     payment_info: {
       type: Object,
-      }
-}, {
-  timestamps: true
-})
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const OrderModel: Model<IOrder> = mongoose.model("Order", orderSchema);
-export default OrderModel
+export default OrderModel;
