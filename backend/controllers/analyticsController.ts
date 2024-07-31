5af26fc3ef6fec4307a33951dc4b56596f35e96a
delaquash <olaide1191@gmail.com>
2024-07-31 09:05:33 +0100

    feat:(coursemodel add timestamp

--- backend/controllers/analyticsController.ts
+++ backend/controllers/analyticsController.ts
@@ -6,40 +6,44 @@ import { generateLast12MonthsData } from "../utils/analytics.generator";
 import CourseModel from "../models/CourseModel";
 import OrderModel from "../models/OrderModel";
 
-export const generateUserAnalytics = CatchAsyncError(async(req: Request, res: Response, next:NextFunction)=> {
+export const generateUserAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const user = generateLast12MonthsData(User)
-        res.status(200).json({
-            success: true,
-            user
-        })
+      const user = generateLast12MonthsData(User);
+      res.status(200).json({
+        success: true,
+        user,
+      });
     } catch (error: any) {
-        return next(new ErrorHandler(error.message, 500));
-      }
-})
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
 
-export const generateCourseAnalytics = CatchAsyncError(async(req: Request, res: Response, next:NextFunction)=> {
+export const generateCourseAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const course = generateLast12MonthsData(CourseModel)
-        res.status(200).json({
-            success: true,
-            course
-        })
+      const course = generateLast12MonthsData(CourseModel);
+      res.status(200).json({
+        success: true,
+        course,
+      });
     } catch (error: any) {
-        return next(new ErrorHandler(error.message, 500));
-      }
-})
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
 
-
-
-export const generateOrderAnalytics = CatchAsyncError(async(req: Request, res: Response, next:NextFunction)=> {
+export const generateOrderAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const order = generateLast12MonthsData(OrderModel)
-        res.status(200).json({
-            success: true,
-            order
-        })
+      const order = generateLast12MonthsData(OrderModel);
+      res.status(200).json({
+        success: true,
+        order,
+      });
     } catch (error: any) {
-        return next(new ErrorHandler(error.message, 500));
-      }
-})
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
--- backend/route/userRoute.ts
+++ backend/route/userRoute.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import {
-    deleteUser, 
-    activateUser,
-    updateUserByAdmin,
-    getAllUsers, 
-    getUserInfo, 
-    RegisterUser,  
-    updateAccessToken, 
-    updatePassword,
-    updateProfilePicture, 
-    updateUserInfo 
+  deleteUser,
+  activateUser,
+  updateUserByAdmin,
+  getAllUsers,
+  getUserInfo,
+  RegisterUser,
+  updateAccessToken,
+  updatePassword,
+  updateProfilePicture,
+  updateUserInfo,
 } from "../controllers/userController";
 
 import { ExpressValidator } from "../middleware/validation";
@@ -22,10 +22,25 @@ router.post("/activate-user", activateUser);
 router.get("/refresh_token", updateAccessToken);
 router.get("/me", isUserAuthenticated, getUserInfo);
 router.put("/update-user-info", isUserAuthenticated, updateUserInfo);
-router.put("/update-user-password", isUserAuthenticated, updatePassword)
-router.put("/update-profile-avatar", isUserAuthenticated, updateProfilePicture)
-router.get("/get-all-user", isUserAuthenticated,authorization("admin"), getAllUsers)
-router.put("/admin-update-user", isUserAuthenticated,authorization("admin"), updateUserByAdmin);
-router.delete("/delete-user/:id", isUserAuthenticated,authorization("admin"), deleteUser);
+router.put("/update-user-password", isUserAuthenticated, updatePassword);
+router.put("/update-profile-avatar", isUserAuthenticated, updateProfilePicture);
+router.get(
+  "/get-all-user",
+  isUserAuthenticated,
+  authorization("admin"),
+  getAllUsers
+);
+router.put(
+  "/admin-update-user",
+  isUserAuthenticated,
+  authorization("admin"),
+  updateUserByAdmin
+);
+router.delete(
+  "/delete-user/:id",
+  isUserAuthenticated,
+  authorization("admin"),
+  deleteUser
+);
 
 export default router;
