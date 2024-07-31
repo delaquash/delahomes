cfa69806c8c0b9d4c710ff7d36d260071437b6cc
delaquash <olaide1191@gmail.com>
2024-07-31 09:05:33 +0100

    feat:(coursemodel add timestamp

--- backend/route/analyticsRouter.ts
+++ backend/route/analyticsRouter.ts
@@ -1,9 +1,28 @@
 import express from "express";
 import { isUserAuthenticated, authorization } from "../middleware/auth";
-import { generateCourseAnalytics, generateOrderAnalytics, generateUserAnalytics } from "../controllers/analyticsController";
+import {
+  generateCourseAnalytics,
+  generateOrderAnalytics,
+  generateUserAnalytics,
+} from "../controllers/analyticsController";
 
 const router = express.Router();
-router.get("/get-user-analytics", isUserAuthenticated, authorization("admin"), generateUserAnalytics);
-router.get("/get-course-analytics", isUserAuthenticated, authorization("admin"), generateCourseAnalytics);
-router.get("/get-order-analytics", isUserAuthenticated, authorization("admin"), generateOrderAnalytics);
+router.get(
+  "/get-user-analytics",
+  isUserAuthenticated,
+  authorization("admin"),
+  generateUserAnalytics
+);
+router.get(
+  "/get-course-analytics",
+  isUserAuthenticated,
+  authorization("admin"),
+  generateCourseAnalytics
+);
+router.get(
+  "/get-order-analytics",
+  isUserAuthenticated,
+  authorization("admin"),
+  generateOrderAnalytics
+);
 export default router;
