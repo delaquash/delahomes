f7da99f8c6a4983988de043416c4a130f5de140a
delaquash <olaide1191@gmail.com>
2024-08-01 09:40:08 +0100

    feat: (layoutRoute) add edit layout route

--- backend/route/layoutRoute.ts
+++ backend/route/layoutRoute.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { authorization, isUserAuthenticated } from "../middleware/auth";
-import { createLayout } from "../controllers/LayoutController";
+import { createLayout, editLayout } from "../controllers/LayoutController";
 
 const router = express.Router();
-router.post("/create-layout",isUserAuthenticated, authorization("admin"), createLayout)
+router.post("/create-layout",isUserAuthenticated, authorization("admin"), createLayout);
+router.post("/edit-layout",isUserAuthenticated, authorization("admin"), editLayout);
 export default router;
