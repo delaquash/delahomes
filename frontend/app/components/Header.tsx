cfda4307f8a1ba54893fd3542777b99f808a00dc
delaquash <olaide1191@gmail.com>
2024-08-07 12:58:39 +0100

    "Added Link component from 'next/link' and modified Header component structure and styles"

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link";
 import React, { FC, useState } from "react";
 
 type Props = {
@@ -6,8 +7,6 @@ type Props = {
     setOpen: (open: boolean) => void;
     activeItem: number;
 }
-
-
 
 const Header:FC<Props> = () => {
     const [active, setActive] = useState(false)
@@ -31,7 +30,18 @@ const Header:FC<Props> = () => {
                 ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500" 
                 : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"}`}
             >
-
+                <div className="m-auto py-2 h-full w-[95%] 800px:w-[92%]">
+                    <div className="w-full h-[80px] flex items-center justify-between p-3">
+                        <div>
+                            <Link 
+                                className={`text-[25px] font-Poppins font-[500] text-black dark:te`}
+                                href={"/"}
+                            >
+                                Delacourse
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
             Header
         </div>
