15c286e600e349969a9e08f56cc5bf677ac98a00
delaquash <olaide1191@gmail.com>
2024-08-07 16:18:42 +0100

    "Updated ThemeSwitcher component to use useTheme hook and added conditional rendering of BiMoon and BiSun icons based on theme"

--- frontend/app/utils/ThemeSwitcher.tsx
+++ frontend/app/utils/ThemeSwitcher.tsx
@@ -6,7 +6,7 @@ import { BiMoon, BiSun } from "react-icons/bi"
 
 const ThemeSwitcher = () => {
     const [mounted, setMounted] = useState(false)
-    const [theme, setTheme] = useState();
+    const {theme, setTheme} = useTheme();
 
     useEffect(()=> setMounted(true), [])
 
@@ -14,7 +14,20 @@ const ThemeSwitcher = () => {
 
   return (
     <div className="flex items-center justify-center mx-4">
-      
+      {theme === "light" ? (
+        <BiMoon
+            className="cursor pointer"
+            onClick={() => setTheme("dark")}
+            fill="black"
+            size={25}
+        />
+      ) : (
+        <BiSun
+            size={25}
+            className="cursor-pointer"
+            onClick={()=>setTheme("light")}
+        />
+      )}
     </div>
   )
 }
