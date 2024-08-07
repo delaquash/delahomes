2d01e1582c48efb3f18d887e8b173b17a0373982
delaquash <olaide1191@gmail.com>
2024-08-07 17:26:22 +0100

    "Updated Header and NavItems components with new icons and styling, and refactored code for better readability."

--- frontend/app/components/NavItems.tsx
+++ frontend/app/components/NavItems.tsx
@@ -1,69 +1,75 @@
 // "use client"
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 export const navItemsData = [
-    {
-        name: "Home",
-        url: "/"
-    },
-    {
-        name: "Courses",
-        url: "/courses"
-    },
-    {
-        name: "About",
-        url: "/about"
-    },
-    {
-        name: "Policy",
-        url: "/url"
-    },
-    {
-        name: "FAQ",
-        url: "/faq  "
-    }
-]
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Courses",
+    url: "/courses",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Policy",
+    url: "/url",
+  },
+  {
+    name: "FAQ",
+    url: "/faq  ",
+  },
+];
 
 type Props = {
-    activeItem: number,
-    isMobile: boolean
-}
-const NavItems:React.FC<Props> = ({activeItem,isMobile }) => {
+  activeItem: number;
+  isMobile: boolean;
+};
+const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
   return (
     <>
-    <div className="hidden 800px:flex">
-        {navItemsData && navItemsData.map((data, index)=>(
+      <div className="hidden 800px:flex">
+        {navItemsData &&
+          navItemsData.map((data, index) => (
             <Link href={`${data.url}`} key={index} passHref>
-                <span
-                    className={`${activeItem === index 
-                        ? "dark:text-[#37a39a] text:-[crimson]"
-                        :" dark:text-white text-black"} text-[10px] px-6 font-Poppins font-[400]`}
-                >
-                    {data.name}
-                </span>
+              <span
+                className={`${
+                  activeItem === index
+                    ? "dark:text-[#37a39a] text:-[crimson]"
+                    : " dark:text-white text-black"
+                } text-[10px] px-6 font-Poppins font-[400]`}
+              >
+                {data.name}
+              </span>
             </Link>
-        ))}
-    </div>
-    {isMobile && (
+          ))}
+      </div>
+      {isMobile && (
         <div className="800px:hidden mt-5">
-            <div className="w-full text-center py-6">
-            {navItemsData && navItemsData.map((data, index)=>(
-            <Link href={`${data.url}`} key={index} passHref>
-                <span
-                    className={`${activeItem === index 
+          <div className="w-full text-center py-6">
+            {navItemsData &&
+              navItemsData.map((data, index) => (
+                <Link href={`${data.url}`} key={index} passHref>
+                  <span
+                    className={`${
+                      activeItem === index
                         ? "dark:text-[#37a39a] text:-[crimson]"
-                        :" dark:text-white text-black"} block text-[10px] py-6 font-Poppins font-[400]`}
-                >
+                        : " dark:text-white text-black"
+                    } block text-[10px] py-6 font-Poppins font-[400]`}
+                  >
                     {/* {data.name} */}
-                </span>
-            </Link>
-        ))}
-            </div>
+                  </span>
+                </Link>
+              ))}
+          </div>
         </div>
-    )}
+      )}
     </>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
