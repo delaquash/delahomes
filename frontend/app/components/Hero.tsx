1d3c2467486d4d86fbd4ce3d65c8281ba0fec692
delaquash <olaide1191@gmail.com>
2024-08-09 21:47:17 +0100

    fix: (hero) add more styling

--- frontend/app/components/Hero.tsx
+++ frontend/app/components/Hero.tsx
@@ -22,6 +22,7 @@ const Hero: FC<Props> = (props) => {
                         className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
                     />
                 </div>
+
                 <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
                     {/* css not finished */}
                     <h2 className="dark:text-white text:-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[]">
@@ -37,8 +38,8 @@ const Hero: FC<Props> = (props) => {
                         <input 
                             type="search" 
                             placeholder="Search Courses..."
-                            // css no finished
-                            className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none"
+                           
+                            className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004c] dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin"
                         />
                         <div className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]">
                             <BiSearch 
@@ -71,10 +72,13 @@ const Hero: FC<Props> = (props) => {
                         <p className="font-Josefin dark:text-[#edfff4] text-[#00000b3] 1000px:pl-3 text-[10px] font-[600]">
                             500k+ People already trusted us.{" "}
                             <Link href="/courses" className="dark:text-[#46e254] text-[crimson]">
+                                View Courses
                             </Link>
                         </p>
                     </div>
+                    <br />
                 </div>
+
             </div>
         </div>
     )
