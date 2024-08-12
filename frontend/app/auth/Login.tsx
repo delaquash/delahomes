cf94ed7d0756f6d54602ee389c33243cf00cc124
delaquash <olaide1191@gmail.com>
2024-08-12 14:46:43 +0100

    "Updated Login.tsx and styles.ts: refactored code, added password input field with show/hide functionality, and modified styles"

--- frontend/app/auth/Login.tsx
+++ frontend/app/auth/Login.tsx
@@ -1,54 +1,81 @@
-"use client"
-import { useFormik } from 'formik';
-import React, { FC, useState } from 'react';
+"use client";
+import { useFormik } from "formik";
+import React, { FC, useState } from "react";
 import * as Yup from "yup";
-import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
-import { styles } from "../styles/styles";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { style } from "../styles/styles";
 
 type Props = {
-    setRoute: (route: string) => void;
-}
+  setRoute: (route: string) => void;
+};
 
 const YupSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Please enter your email!'),
-    password: Yup.string().required('Please enter your password!').min(6)
-})
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Please enter your email!"),
+  password: Yup.string().required("Please enter your password!").min(6),
+});
 
-const Login:FC<Props> = (props: Props) => {
-    const [show, setShow] = useState(false);
+const Login: FC<Props> = (props: Props) => {
+  const [show, setShow] = useState(false);
 
-    const formik = useFormik({
-        initialValues: {
-            email: "", password: ""
-        },
-        validationSchema: YupSchema,
-        onSubmit: async({ email, password}) => {
-            console.log(email, password)
-        }
-    });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: YupSchema,
+    onSubmit: async ({ email, password }) => {
+      console.log(email, password);
+    },
+  });
 
-    const { errors, touched, values, handleChange, handleSubmit} = formik;
+  const { errors, touched, values, handleChange, handleSubmit } = formik;
   return (
-    <div className='w-full'>
-        <h1 className={`${styles.title}`}>
-            Learning with DelaCourse
-        </h1>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email" className={`${styles.label}`}>
-                Enter your Email
-            </label>
-            <input 
-                type="email"
-                id="email"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-                placeholder='Please type your email'
-                className={`${errors.email && touched.email && "border-red-500"} w-full text-black dark:text-white bg-transparent rounded h-[40px] px-2 outline-none mt-[10px font-Poppins]`}
-            />
-        </form>
+    <div className="w-full">
+      <h1 className={`${style.title}`}>Learning with DelaCourse</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email" className={`${style.label}`}>
+          Enter your Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          placeholder="Please type your email"
+          className={`${errors.email && touched.email && "border-red-500"} ${
+            style.input
+          }`}
+        />
+        {errors.email && touched.email && (
+          <span className="text-red-500 pt-2 block">{errors.email}</span>
+        )}
+        <div className="w-full mt-5 relative mb-1">
+          <label htmlFor="password" className={`${style.label}`}>
+            Enter your Password
+          </label>
+          <input
+            type={!show ? "password" : "text"}
+            id="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            placeholder="Please type your password"
+            className={`${
+              errors.password && touched.password && "border-red-500"
+            } ${style.input}`}
+          />
+          {!show ? (
+            <AiOutlineEyeInvisible size={20} onClick={() => setShow(true)} />
+          ) : (
+            <AiOutlineEye size={20} onClick={() => setShow(true)} />
+          )}
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
