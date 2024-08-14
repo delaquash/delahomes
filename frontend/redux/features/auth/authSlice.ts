9e3965013bb6d107bfb4077d12cf0eafb7619f2b
delaquash <olaide1191@gmail.com>
2024-08-14 16:03:00 +0100

    feat: (auth) add state for api

--- frontend/redux/features/auth/authSlice.ts
+++ frontend/redux/features/auth/authSlice.ts
@@ -5,3 +5,24 @@ const initialState = {
     token: "",
     user: ""
 }
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        userRegistration: (state, action) => {
+            state.token = action.payload.token
+        },
+        userLoggedIn: (state, action)=> {
+            state.user = action.payload.user,
+            state.token = action.payload.accessToken
+        },
+        userLoggedOut: (state, action) => {
+            state.token = "",
+            state.user = ""
+        }
+    }
+})
+
+export const {userLoggedIn, userLoggedOut, userRegistration} = authSlice.actions
