a7632cfe9ed77efb4fe100eb11155898bb5e51e1
delaquash <olaide1191@gmail.com>
2024-08-17 15:31:51 +0100

    "Added Redux state selection in Header component, implemented refreshToken and loadUser endpoints in apiSlice, and initialized app with refreshToken and loadUser dispatches in store"

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -3,8 +3,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./features/api/apiSlice";
 import  authSlice  from "./features/auth/authSlice";
 
-
-
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
@@ -13,3 +11,10 @@ export const store = configureStore({
     devTools: false,
     middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
 })
+
+const initializeApp = async() => {
+    await store.dispatch(apiSlice.endpoints.refreshToken.initiate({}, {forceRefetch: true}));
+    await store.dispatch(apiSlice.endpoints.loadUser.initiate({}, {forceRefetch: true}))
+}
+
+initializeApp();
