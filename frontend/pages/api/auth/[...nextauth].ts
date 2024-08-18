efcb463d9997100809993c0e6bf1dc000c1b61ea
delaquash <olaide1191@gmail.com>
2024-08-18 13:19:55 +0100

    "Updated .env file with new environment variables and added next-auth dependency to package.json"

---
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import TwitterProvider from "next-auth/providers/twitch";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
+
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID || "",
+      clientSecret: process.env.TWITTER_SECRET || "",
+    }),
+  ],
+  secret: process.env.SECRET
+};
+
+export default NextAuth(authOptions)
