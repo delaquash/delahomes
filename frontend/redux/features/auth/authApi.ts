58d727e4511e94743119de1771d198ed59d760c8
delaquash <olaide1191@gmail.com>
2024-08-19 07:25:20 +0100

    "Formatted code in authController.ts and authApi.ts files, added whitespace and corrected indentation."

--- frontend/redux/features/auth/authApi.ts
+++ frontend/redux/features/auth/authApi.ts
@@ -1,97 +1,101 @@
 import { apiSlice } from "../api/apiSlice";
-import { userLoggedIn, userRegistration  } from "./authSlice";
+import { userLoggedIn, userRegistration } from "./authSlice";
 
 type RegistrationResponse = {
-    message: string;
-    activationToken: string
-}
+  message: string;
+  activationToken: string;
+};
 
 type RegistrationData = {};
 
 export const authApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        // endpoints here
-        register: builder.mutation<RegistrationResponse, RegistrationData>({
-            query: (data) => ({
-                url: "http://localhost:5000/api/v1/auth/registeruser",
-                method: "POST",
-                body: data,
-                credentials: "include" as const,
-            }),
-            async onQueryStarted(arg, {queryFulfilled, dispatch}){
-                try {
-                    const result = await queryFulfilled;
-                    dispatch(
-                        userRegistration({
-                            token: result.data.activationToken
-                        })
-                    )
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-        }),
-        activation: builder.mutation({
-            query:({activation_token, activation_code}) => ({
-                url: "http://localhost:5000/api/v1/auth/activate-user",
-                method: "POST",
-                body: {
-                    activation_token, 
-                    activation_code
-                },
+  endpoints: (builder) => ({
+    // endpoints here
+    register: builder.mutation<RegistrationResponse, RegistrationData>({
+      query: (data) => ({
+        url: "http://localhost:5000/api/v1/auth/registeruser",
+        method: "POST",
+        body: data,
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userRegistration({
+              token: result.data.activationToken,
             })
-        }),
-        login: builder.mutation({
-            query:({email, password}) => ({
-                url: "http://localhost:5000/api/v1/auth/signin",
-                method: "POST",
-                body: {
-                    email,
-                    password
-                },
-                credentials: "include" as const
-            }),
-            async onQueryStarted(arg, {queryFulfilled, dispatch}){
-                try {
-                    const result = await queryFulfilled;
-                    dispatch(
-                        userLoggedIn({
-                            accessToken: result.data.accessToken,
-                            user: result.data.user
-                        })
-                    )
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-        }),
-        socialAuth: builder.mutation({
-            query:({ email, name, avatar}) => ({
-                url: "http://localhost:5000/api/v1/auth/social-auth",
-                method: "POST",
-                body: {
-                    email,
-                    name,
-                    avatar
-                },
-                credentials: "include" as const
-            }),
-            async onQueryStarted(arg, {queryFulfilled, dispatch}){
-                try {
-                    const result = await queryFulfilled;
-                    dispatch(
-                        userLoggedIn({
-                            accessToken: result.data.accessToken,
-                            user: result.data.user
-                        })
-                    )
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-        })
-    })
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+    activation: builder.mutation({
+      query: ({ activation_token, activation_code }) => ({
+        url: "http://localhost:5000/api/v1/auth/activate-user",
+        method: "POST",
+        body: {
+          activation_token,
+          activation_code,
+        },
+      }),
+    }),
+    login: builder.mutation({
+      query: ({ email, password }) => ({
+        url: "http://localhost:5000/api/v1/auth/signin",
+        method: "POST",
+        body: {
+          email,
+          password,
+        },
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+    socialAuth: builder.mutation({
+      query: ({ email, name, avatar }) => ({
+        url: "http://localhost:5000/api/v1/auth/social-auth",
+        method: "POST",
+        body: {
+          email,
+          name,
+          avatar,
+        },
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  }),
 });
 
-
-export const { useRegisterMutation, useActivationMutation, useLoginMutation } = authApi
+export const {
+  useRegisterMutation,
+  useActivationMutation,
+  useLoginMutation,
+  useSocialAuthMutation,
+} = authApi;
