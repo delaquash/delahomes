24a49cc3048a847efe36eb85e6ead7f6ac43793c
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-08-29 09:03:00 -0800

    "Added Heading component to admin page and removed simple 'page' div"

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Heading from '../utils/Heading';
+
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>page</div>
+    <Heading 
+        description='An online platform where student can learn and interact with a teachers and get real time solution'
+        title='ELearning -- Admin'
+        keywords='Programming, Software Engineering, Machine Learning, JavaScript, Python'
+    />
   )
 }
 
