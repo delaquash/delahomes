06078e15b3f70415e365a578835b65a91edd40c6
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-08-29 09:59:44 -0800

    "Added AdminSideBar component and modified page layout in admin/page.tsx"

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import Heading from '../utils/Heading';
+import AdminSideBar from '../components/admin/sidebar/AdminSideBar';
 
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <Heading 
+    <div>
+
+          <Heading 
         description='An online platform where student can learn and interact with a teachers and get real time solution'
         title='ELearning -- Admin'
         keywords='Programming, Software Engineering, Machine Learning, JavaScript, Python'
     />
+    <div className="flex h-[20px]">
+        <div className="1500px:w-[16%] w-1/5">
+            <AdminSideBar />
+        </div>
+        <div className="w-[85%]">
+
+        </div>
+    </div>
+  
+
+    </div>
   )
 }
 
---
+++ frontend/app/components/admin/sidebar/AdminSideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const AdminSideBar = (props: Props) => {
+  return (
+    <div>AdminSideBar</div>
+  )
+}
+
+export default AdminSideBar
