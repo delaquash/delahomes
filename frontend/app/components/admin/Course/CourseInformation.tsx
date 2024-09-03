942618c69817008549d6cd3648b5638679df63d7
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-09-02 17:12:57 -0800

    feat(course info) add tags and estimated price

--- frontend/app/components/admin/Course/CourseInformation.tsx
+++ frontend/app/components/admin/Course/CourseInformation.tsx
@@ -1,21 +1,26 @@
 "use client";
-import { styles } from '@/app/styles/style';
-import React, { useState } from 'react'
+import { styles } from "@/app/styles/style";
+import React, { useState } from "react";
 
 type Props = {
-    courseInfo:any;
-    setCourseInfo: (course: any)=> void;
-    active: number;
-    setActive: (active: number)=> void;
-}
+  courseInfo: any;
+  setCourseInfo: (course: any) => void;
+  active: number;
+  setActive: (active: number) => void;
+};
 
-const CourseInformation =({ courseInfo, setCourseInfo, active, setActive}: Props) => {
-    const [dragging, setDragging] = useState(false)
+const CourseInformation = ({
+  courseInfo,
+  setCourseInfo,
+  active,
+  setActive,
+}: Props) => {
+  const [dragging, setDragging] = useState(false);
 
-    const handleSubmit= (e: any) => {
-        e.preventDefault();
-        setActive(active+1)
-    }
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setActive(active + 1);
+  };
   return (
     <div className="w-[80%] m-auto mt-24">
       <form className={`${styles.label}`} onSubmit={handleSubmit}>
@@ -37,36 +42,69 @@ const CourseInformation =({ courseInfo, setCourseInfo, active, setActive}: Props
         <br />
         <div className="mb-5">
           <label className={`${styles.label}`}>Course Registration</label>
-          <textarea 
-            placeholder="Write something amazing..." 
-            name="" 
-            rows={10} 
+          <textarea
+            placeholder="Write something amazing..."
+            name=""
+            rows={10}
             cols={30}
             value={courseInfo.description}
             className={`${styles.label} !h-min !py-2`}
-            onChange={(e:any)=>setCourseInfo({...courseInfo, description:e.target.value})}
+            onChange={(e: any) =>
+              setCourseInfo({ ...courseInfo, description: e.target.value })
+            }
           ></textarea>
         </div>
         <br />
         <div className="w-full flex justify-between">
-            <div className="w-[45%]">
-                <label className={`${styles.label}`}>Course price</label>
-                <input 
-                    type='number'
-                    required
-                    value={courseInfo.price}
-                    onChange={(e: any) =>
-                    setCourseInfo({ ...courseInfo, price: e.target.value })
-                    }
-                    id="price"
-                    placeholder='30'
-                    className={`${styles.input}`}
-                />
-            </div>
+          <div className="w-[45%]">
+            <label className={`${styles.label}`}>Course price</label>
+            <input
+              type="number"
+              required
+              value={courseInfo.price}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, price: e.target.value })
+              }
+              id="price"
+              placeholder="30"
+              className={`${styles.input}`}
+            />
+          </div>
+
+          <div className="w-[50%]">
+            <label className={`${styles.label}`}>Estimated Course Price</label>
+            <input
+              type="number"
+              required
+              name=""
+              value={courseInfo.estimatedPrice}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, estimatedPrice: e.target.value })
+              }
+              placeholder="30"
+              className={`${styles.input}`}
+              id="price"
+            />
+          </div>
+        </div>
+        <div>
+        <label className={`${styles.label}`} htmlFor="email">Course Tags</label>
+        <input
+              type="text"
+              required
+              name=""
+              value={courseInfo.tags}
+              onChange={(e: any) =>
+                setCourseInfo({ ...courseInfo, tags: e.target.value })
+              }
+              placeholder="Software Dev, MERN, Tailwind, CSS, Web, ReactJS, React Native"
+              className={`${styles.input}`}
+              id="tags"
+            />
         </div>
       </form>
     </div>
   );
-}
+};
 
-export default CourseInformation
+export default CourseInformation;
