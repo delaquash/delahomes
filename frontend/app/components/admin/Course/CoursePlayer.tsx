104c29e75b86c1a416ecf07e4c9887d5ccd67074
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-09-12 14:54:08 -0800

    "Updated CoursePlayer component to import useEffect and axios, added Props type with videoUrl and title, and introduced state for videoData"

--- frontend/app/components/admin/Course/CoursePlayer.tsx
+++ frontend/app/components/admin/Course/CoursePlayer.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from "axios";
 
-type Props = {}
+type Props = {
+    videoUrl: string;
+    title: string;
+}
 
-const CoursePlayer = (props: Props) => {
+const CoursePlayer = ({title, videoUrl}: Props) => {
+    const [videoData, setVideoData] = useState({
+        otp:"",
+        playnackInfo: ""
+    })
   return (
     <div>CoursePlayer</div>
   )
