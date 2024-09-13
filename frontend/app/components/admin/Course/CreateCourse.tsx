260623109fa5211a7bd2f448f8f7c8c249c79a52
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-09-13 13:57:45 -0800

    "Added useEffect hook to handle create course mutation response, toast notifications, and redirect; updated handleCourseCreate function to be async and check for isLoading"

--- frontend/app/components/admin/Course/CreateCourse.tsx
+++ frontend/app/components/admin/Course/CreateCourse.tsx
@@ -1,18 +1,31 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CourseInformation from './CourseInformation'
 import CourseOptions from './CourseOptions'
 import CourseData from './CourseData'
 import CourseContent from './CourseContent'
 import CoursePreview from './CoursePreview'
 import { useCreateCourseMutation } from '@/redux/features/course/coursesApi'
+import toast from 'react-hot-toast'
+import { redirect } from 'next/navigation'
 
 type Props = {}
 
 const CreateCourse = (props: Props) => {
     const [createCourse, {isSuccess, error, isLoading}] = useCreateCourseMutation()
 
-    useEff
+    useEffect(() => {
+        if (isSuccess) {
+            toast.success("Courses created successfully...")
+            redirect("/admin/all-courses")
+            }
+            if(error){
+                if("data" in error){
+                    const errorMessage = error as any  
+                    toast.error(errorMessage.data.message)     
+                }          
+            }
+    }, [isLoading, isSuccess, error])
     const [active, setActive] = useState(2)
     const [courseInfo, setCourseInfo] = useState({
         name: "",
@@ -77,9 +90,11 @@ const CreateCourse = (props: Props) => {
         }
     }
 
-    const handleCourseCreate = (e: any) => {
-        const data = courseData
-        createCourse(data)
+    const handleCourseCreate =async (e: any) => {
+        const data = courseData;
+        if(!isLoading){
+            await createCourse(data)
+        }
     }
     
   return (
