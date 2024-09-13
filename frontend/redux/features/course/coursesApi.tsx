2a4bfb79589b0ec5aa8556a0ccebcb1280cb2f0f
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-09-13 13:07:15 -0800

    "Updated coursesApi.tsx: changed URL and body of createCourse mutation, added export for useCreateCourseMutation"

--- frontend/redux/features/course/coursesApi.tsx
+++ frontend/redux/features/course/coursesApi.tsx
@@ -4,13 +4,14 @@ import { apiSlice } from "../api/apiSlice";
 export const courseApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         createCourse: builder.mutation({
-            query: ({data}) => ({
-                url: "http://localhost:5000/api/v1/courses/create-course",
+            query: ({ data }) => ({
+                url: "http://localhost:5000/api/v1/course/create-course",
                 method: "POST",
-                body: data,
+                body:{ data },
                 credentials: "include" as const
                 })
         })
     })
 })
 
+export const {useCreateCourseMutation} = courseApi
