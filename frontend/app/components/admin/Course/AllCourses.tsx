1a1152d6d70b7284c41ab5df4ab4773e47055520
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-09-13 16:02:38 -0800

    "Updated dependencies in package-lock.json and package.json, adding @mui/x-data-grid and updating @babel/runtime"

---
+++ frontend/app/components/admin/Course/AllCourses.tsx
@@ -0,0 +1,14 @@
+import { useTheme } from '@emotion/react';
+import { Box, Button } from '@mui/material';
+import React from 'react';
+import { AiOutlineDelete } from 'react-icons/ai';
+
+type Props = {}
+
+const AllCourses = (props: Props) => {
+  return (
+    <div>AllCourses</div>
+  )
+}
+
+export default AllCourses
