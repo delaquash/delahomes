697d4eda628227333e23281d3f6a11ab89c1c612
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-09-17 11:57:08 +0100

    "Updated auth middleware to decode access token without verification, added check for expired access token and refresh token logic"

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -4,6 +4,7 @@ import  ErrorHandler  from '../utils/errorHandler';
 import { CatchAsyncError } from './CatchAsyncError';
 import { redis } from '../utils/redis';
 import { IUser } from '../types/ModelTypes/UserModel';
+import { updateAccessToken } from '../controllers/userController';
 
 // Extend the Express Request interface to include the user property
 declare global {
@@ -20,17 +21,27 @@ export const isUserAuthenticated= CatchAsyncError(async(req:Request, res: Respon
   if(!access_token){
     return next(new ErrorHandler("Please login to access this resources", 400))
   }
-  const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN as string) as JwtPayload
+  const decoded = jwt.decode(access_token) as JwtPayload
   if(!decoded) {
     return next(new ErrorHandler("access token not valid", 400))
   }
-  const user = await redis.get(decoded.id);
-  // console.log(user, "this user is from redis")
-  if(!user) {
-    return next(new ErrorHandler("User not found! Please sign up to access this resources", 400))
+
+  // check if the access token is expired
+  if(decoded.exp && decoded.exp <= Date.now() / 1000) {
+    try {
+      await updateAccessToken(req, res, next)
+    } catch (error) {
+      return next(error)
+    }
+  } else {
+    const user = await redis.get(decoded.id);
+    // console.log(user, "this user is from redis")
+    if(!user) {
+      return next(new ErrorHandler("User not found! Please sign up to access this resources", 400))
+    }
+    req.user = JSON.parse(user);
+    next();
   }
-  req.user = JSON.parse(user);
-  next();
 })
 
 
