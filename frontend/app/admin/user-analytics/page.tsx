d6c22d6d1b13322711deada64a69ca89ac379614
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-09-28 12:04:29 +0100

    feat: (user analytics) add user chart analytics

---
+++ frontend/app/admin/user-analytics/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import Heading from "../../utils/Heading";
+import AdminSideBar from "../../components/admin/sidebar/AdminSideBar";
+import DashboardHero from "../../components/admin/DashboardHero";
+import AdminProtected from "../../hooks/useAdminProtected";
+import UserAnalytics from "../../components/Analytics/UserAnalytics";
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <AdminProtected>
+      <Heading
+        description="An online platform where student can learn and interact with a teachers and get real time solution"
+        title="ELearning -- Admin"
+        keywords="Programming, Software Engineering, Machine Learning, JavaScript, Python"
+      />
+      <div className="flex h-[200vh]">
+        <div className="1500px:w-[16%] w-1/5">
+          <AdminSideBar />
+        </div>
+        <div className="w-[85%]">
+          <DashboardHero />
+          <UserAnalytics />
+        </div>
+      </div>
+      </AdminProtected>
+    </div>
+  );
+};
+
+export default page;
---
+++ frontend/app/components/Analytics/UserAnalytics.tsx
@@ -0,0 +1,86 @@
+import { styles } from '@/app/styles/style';
+import { useGetUserAnalyticsQuery } from '@/redux/features/analytics/analyticsApi';
+import React from 'react';
+import {
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    Tooltip,
+    ResponsiveContainer
+} from "recharts";
+import Loader from '../Loader/Loader';
+
+type Props = {
+    isDashboard?: boolean
+}
+
+const UserAnalytics = ({isDashboard}: Props) => {
+    const {data, isLoading } = useGetUserAnalyticsQuery({})
+
+        const analyticsData = [
+        {name: "Jan 2023", count: 2001},
+        {name: "Feb 2023", count: 4001},
+        {name: "March 2023", count: 6001},
+        {name: "April 2023", count: 8001},
+        {name: "May 2023", count: 1001},
+        {name: "Jun 2023", count: 5001},
+        {name: "July 2023", count: 7001},
+        {name: "Aug 2023", count: 7001},
+        {name: "Sept 2023", count: 7001},
+        {name: "Oct 2023", count: 7001},
+        {name: "Nov 2023", count: 7001},
+        {name: "December 2023", count: 7001},
+    ]
+
+    // This is the actual data from api, the one above is just hardcoded
+    // const analyticsData: any = []
+
+    // data && data.courses.last12Months.forEach((analysis: any)=> {
+    //     analyticsData.push({ name: analysis.month, uv: analysis.count })
+    // });
+  return (
+    <>
+        {isLoading ? (
+            <Loader />
+        ) : (
+            <div className={`${!isDashboard ? "mt-[50px]" :"mt-[50px] dark:bg-[#111C43] shadow-sm pb-5 rounded-sm"}`}>
+                <div className={`${isDashboard ? "!ml-8 mb-5": ""}`}>
+                    <h1 className={`${styles.title} ${isDashboard && "!text-[20px]"} px-5 !text-start`}> User Analytics</h1>
+                    {!isDashboard && (
+                        <p className={`${styles.label} px-5`}>Last 12 months analytics data {" "}</p>
+                    )}
+                </div>
+                    <div className={`w-full ${isDashboard ? "h-[30vh]" : "h-screen"} flex items-center justify-center`}>
+                        <ResponsiveContainer
+                            width={isDashboard ? "100%" : "90%"} height={!isDashboard ? "50%" : "100%"}
+                        >
+                            <AreaChart
+                                data={analyticsData}
+                                margin={{
+                                    top: 20,
+                                    right: 30,
+                                    left: 0,
+                                    bottom: 0
+                                }}
+                            >
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Area 
+                                    type="monotone"
+                                    dataKey="count"
+                                    stroke='#4d62d9'
+                                    fill='4d62d9'
+                                />
+                            </AreaChart>
+                        </ResponsiveContainer>
+
+                    </div>
+            </div>
+        )}
+    </>
+  )
+}
+
+export default UserAnalytics
