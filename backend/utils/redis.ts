84339d896c38ffc329c990bc1048ea69a9bda0f8
delaquash <olaide1191@gmail.com>
2024-11-25 09:59:45 +0100

    fix mobile view

--- backend/utils/redis.ts
+++ backend/utils/redis.ts
@@ -10,4 +10,15 @@ const redisClient = () => {
     throw new Error ("Redis Connection Failed")
 }
 
-export const redis = new Redis(redisClient())
+export const redis = new Redis(redisClient(), {
+    maxRetriesPerRequest: null, 
+    retryStrategy: (times) => {
+        console.log(`[ioredis] Retry attempt: ${times}`);
+        if (times >= 10) {
+          // Stop retrying after 10 attempts
+          console.error("Too many retries, aborting connection.");
+          return null;
+        }
+        return Math.min(times * 100, 2000); // Retry with exponential backoff
+      },
+})
